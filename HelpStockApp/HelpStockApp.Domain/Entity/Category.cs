fc0b1ee07b3da55c53e4f92563506b5beb9bf8dd
icarobellem <icaro.bellem@fatec.sp.gov.br>
2024-09-02 11:04:01 -0300

    Aula 0819 - Added Models in Domain

---
+++ HelpStockApp/HelpStockApp.Domain/Entity/Category.cs
@@ -0,0 +1,17 @@
+﻿namespace HelpStockApp.Domain.Entity
+{
+    public class Category : Entity
+    {
+        public string Name { get; set; }
+
+        public Category(string name)
+        {
+            Name = name; 
+        }
+        public Category(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
---
+++ HelpStockApp/HelpStockApp.Domain/Entity/Entity.cs
@@ -0,0 +1,7 @@
+﻿namespace HelpStockApp.Domain.Entity
+{
+    public class Entity
+    {
+        public int Id { get; protected set; }
+    }
+}
