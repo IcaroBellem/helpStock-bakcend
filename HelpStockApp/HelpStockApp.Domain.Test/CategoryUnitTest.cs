39967eadb7329fc9fa511097a52e01889fd28b14
icarobellem <icaro.bellem@fatec.sp.gov.br>
2024-09-16 11:52:56 -0300

    Added Category Unit Tests

---
+++ HelpStockApp/HelpStockApp.Domain.Test/CategoryUnitTest.cs
@@ -0,0 +1,29 @@
+﻿using HelpStockApp.Domain.Entities;
+using FluentAssertions;
+using Xunit;
+using HelpStockApp.Domain.Validation;
+
+namespace HelpStockApp.Domain.Test
+{
+    #region Teste Positivos de Categoria
+    public class CategoryUnitTest
+    {
+        [Fact(DisplayName = "Create Category With Valid State")]
+        public void Create_Category_WithValidParameters_ResultObjectValidState()
+        {
+            Action action = () => new Category(1, "Category name");
+            action.Should().NotThrow<DomainExceptionValidation>();
+        }
+
+        #endregion
+
+        # region Teste Negativos de Categoria
+        [Fact(DisplayName = "Create Category With Invalid State")]
+        public void Create_Category_WithInvalidParametersId_ResultExeption()
+        {
+            Action action = () => new Category(-1, "Category name");
+            action.Should().Throw<DomainExceptionValidation>().WithMessage("Invalid Id Value");
+        }
+        #endregion
+    }
+}
