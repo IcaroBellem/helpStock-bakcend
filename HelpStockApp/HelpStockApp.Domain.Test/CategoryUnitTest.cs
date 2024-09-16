f788a01fa3dc3589521d942ca0ce632c380907cb
icarobellem <icaro.bellem@fatec.sp.gov.br>
2024-09-16 12:47:33 -0300

    Added New Test Categoty

--- HelpStockApp/HelpStockApp.Domain.Test/CategoryUnitTest.cs
+++ HelpStockApp/HelpStockApp.Domain.Test/CategoryUnitTest.cs
@@ -15,6 +15,14 @@ namespace HelpStockApp.Domain.Test
             action.Should().NotThrow<DomainExceptionValidation>();
         }
 
+
+        [Fact(DisplayName = "Create Category With Category Name Alone Parameters")]
+        public void Create_Category_WithNameALoneParameters_ResultObjectValidState()
+        {
+            Action action = () => new Category("Category name");
+            action.Should().NotThrow<DomainExceptionValidation>();
+        }
+
         #endregion
 
         # region Teste Negativos de Categoria
@@ -24,6 +32,35 @@ namespace HelpStockApp.Domain.Test
             Action action = () => new Category(-1, "Category name");
             action.Should().Throw<DomainExceptionValidation>().WithMessage("Invalid Id Value");
         }
+
+        [Fact(DisplayName = "Create Category With Too Short Parameters")]
+        public void Create_Category_WithTooShortParameters_ResultExeption()
+        {
+            Action action = () => new Category(1, "Ca");
+            action.Should().Throw<DomainExceptionValidation>().WithMessage("Invalid name, too short. minimum 3 characters");
+        }
+
+        [Fact(DisplayName = "Create Category With null Parameters")]
+        public void Create_Category_WithNullParameters_ResultExeption()
+        {
+            Action action = () => new Category(1, null);
+            action.Should().Throw<DomainExceptionValidation>().WithMessage("Invalid name, name is required");
+        }
+
+        [Fact(DisplayName = "Create Category With Name missing Parameters")]
+        public void Create_Category_WithNameMissingParameters_ResultExeption()
+        {
+            Action action = () => new Category(1, "");
+            action.Should().Throw<DomainExceptionValidation>().WithMessage("Invalid name, name is required");
+        }
+
+        [Fact(DisplayName = "Create Category With invalid Id Parameters")]
+        public void Create_Category_WithInvalidIdParameters_ResultExeption()
+        {
+            Action action = () => new Category('a', "Category");
+            action.Should().Throw<DomainExceptionValidation>().WithMessage("Invalid Id");
+        }
+
         #endregion
     }
 }
