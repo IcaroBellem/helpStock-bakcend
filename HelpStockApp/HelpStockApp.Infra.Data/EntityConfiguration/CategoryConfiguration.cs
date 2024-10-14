fa51961e4980df7c8940f541194bcc60ff79f655
icarobellem <icaro.bellem@fatec.sp.gov.br>
2024-10-14 12:31:17 -0300

    Implementação da camada de dominio DbContext

---
+++ HelpStockApp/HelpStockApp.Infra.Data/EntityConfiguration/CategoryConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using HelpStockApp.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelpStockApp.Infra.Data.EntityConfiguration
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category> 
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasKey(t => t.Id);
+            builder.Property(t => t.Name).HasMaxLength(100).IsRequired();
+        }
+    }
+}
