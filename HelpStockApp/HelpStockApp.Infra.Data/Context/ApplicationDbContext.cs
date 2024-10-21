e508b50eee3f05684b0af37b981f5649b6da4706
icarobellem <icaro.bellem@fatec.sp.gov.br>
2024-10-21 10:24:00 -0300

    Implementação das interfaces domain

--- HelpStockApp/HelpStockApp.Infra.Data/Context/ApplicationDbContext.cs
+++ HelpStockApp/HelpStockApp.Infra.Data/Context/ApplicationDbContext.cs
@@ -1,12 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using HelpStockApp.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace HelpStockApp.Infra.Data.Context
 {
-    internal class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
     }
 }
--- HelpStockApp/HelpStockApp.Infra.Data/EntityConfiguration/CategoryConfiguration.cs
+++ HelpStockApp/HelpStockApp.Infra.Data/EntityConfiguration/CategoryConfiguration.cs
@@ -1,15 +1,10 @@
 ﻿using HelpStockApp.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HelpStockApp.Infra.Data.EntityConfiguration
 {
-    public class CategoryConfiguration : IEntityTypeConfiguration<Category> 
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
     {
         public void Configure(EntityTypeBuilder<Category> builder)
         {
