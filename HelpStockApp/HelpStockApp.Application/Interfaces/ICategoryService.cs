4611f7a63cb0633b93666d93408b1b17acd449f7
icarobellem <icaro.bellem@fatec.sp.gov.br>
2024-11-11 12:12:31 -0300

    Added DTOs and Services

---
+++ HelpStockApp/HelpStockApp.Application/Interfaces/ICategoryService.cs
@@ -0,0 +1,18 @@
+﻿using HelpStockApp.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelpStockApp.Application.Interfaces
+{
+    public interface ICategoryService
+    {
+        Task<IEnumerable<CategoryDTO>> GetCategories();
+        Task<CategoryDTO> GetCategorieById(int? id);
+        Task Add(CategoryDTO categoryDto);
+        Task Update(CategoryDTO categoryDto);
+        Task Remove(int? id);
+    }
+}
