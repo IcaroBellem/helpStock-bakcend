73c35f3bcfd3b1fbcdcf0cb2c828218f59eead59
Icaro Bellem <icarobellemskr@hotmail.com>
2024-11-18 21:24:20 -0300

    CRUD Products

---
+++ HelpStockApp/HelpStockApp.API/Controllers/ProductController.cs
@@ -0,0 +1,69 @@
+﻿using HelpStockApp.Application.DTOs;
+using HelpStockApp.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelpStockApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductDTO>>> GetAll()
+        {
+            var products = await _productService.GetProducts();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProductDTO>> GetById(int id)
+        {
+            var product = await _productService.GetProductById(id);
+            if (product == null)
+                return NotFound();
+
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create([FromBody] ProductDTO productDto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            await _productService.Add(productDto);
+            return CreatedAtAction(nameof(GetById), new { id = productDto.Id }, productDto);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Update(int id, [FromBody] ProductDTO productDto)
+        {
+            if (id != productDto.Id)
+                return BadRequest("ID mismatch.");
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            await _productService.Update(productDto);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var product = await _productService.GetProductById(id);
+            if (product == null)
+                return NotFound();
+
+            await _productService.Remove(id);
+            return NoContent();
+        }
+    }
+}
